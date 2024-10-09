fc6b25d68f0fb603e90d74a32fa03d944d1dbe67
ivanildolo <ivanildosilvalima@gmail.com>
2024-10-09 09:37:41 -0300

    feature(Spring-boot)Cria estrutura basica do projeto

---
+++ src/main/java/com/ti/estoque/SistemaDeGerenciamentoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.ti.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ti/estoque/SistemaDeGerenciamentoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerenciamentoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
